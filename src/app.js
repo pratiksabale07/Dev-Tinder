2dbe636274ef6d61823a5cf53cd32ec4b7815a4d
Pratik <pratiksabale.ps@gmail.com>
2025-07-10 22:35:18 +0530

    playing with multiple route handlers

--- src/app.js
+++ src/app.js
@@ -4,23 +4,18 @@ const app = express();
 
 // This will only handle GET call to /user
 app.get("/user", (req, res) => {
-  res.send({ firstName: "Akshay", lastName: "Saini" });
+  res.send({ firstName: "Pratik", lastName: "Sabale" });
 });
 
-app.post("/user", async (req, res) => {
-  console.log(req.body);
-  // saving data to DB
-  res.send("Data successfully saved to the database!");
-});
-
-app.delete("/user", (req, res) => {
-  res.send("Deleted successfully!");
-});
-
-// this will match all the HTTP method API calls to /test
-app.use("/test", (req, res) => {
-  res.send("Hello from the server!");
-});
+app.use('/data', (req, res, next) => { //request handler
+  // res.send('Data 1');
+  next();
+}, (req, res, next) => {
+  // res.send('Data 2');
+  next();
+}, (req, res, next) => {
+  res.send('Data 3');
+})
 
 app.listen(7777, () => {
   console.log("Server is successfully listening on port 7777...");
